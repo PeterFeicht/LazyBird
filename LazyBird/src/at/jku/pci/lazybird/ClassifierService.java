8cef85ea5d6385952684aaa30bc317e5ee2eccf8
Peter Feichtinger <shippo@gmx.at>
2012-12-08 19:54:38 +0000

    LazyBird: -> Added ClassifierService, implemented and untested. -> Added extra strings, broadcast manager and event handler to ReportFragment. -> Implemented reading serialized classifier from internal storage.

---
+++ LazyBird/src/at/jku/pci/lazybird/ClassifierService.java
@@ -0,0 +1,694 @@
+package at.jku.pci.lazybird;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+import at.jku.pci.lazybird.features.Feature;
+import at.jku.pci.lazybird.features.FeatureExtractor;
+import at.jku.pci.lazybird.features.SlidingWindow;
+import at.jku.pci.lazybird.features.SlidingWindow.WindowListener;
+import at.jku.pervasive.sd12.actclient.ClassLabel;
+import at.jku.pervasive.sd12.actclient.CoordinatorClient;
+import weka.classifiers.Classifier;
+import weka.core.Attribute;
+import weka.core.FastVector;
+import weka.core.Instance;
+import weka.core.Instances;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class ClassifierService extends Service implements SensorEventListener, WindowListener
+{
+	private static ClassifierService sInstance = null;
+	
+	public static final String LOGTAG = "ClassifierService";
+	public static final boolean LOCAL_LOGV = true;
+	
+	/**
+	 * Format string to use for log entries, formatted by {@link SimpleDateFormat} and
+	 * {@link String#format(String, Object...)} with one string argument.
+	 */
+	public static final String LOG_FORMAT = "yyyy-MM-dd HH:mm:ss.SSSZ' - %s'";
+	
+	/**
+	 * Format string to use for short log entries, formatted by {@link SimpleDateFormat} and
+	 * {@link String#format(String, Object...)} with one string argument.
+	 */
+	public static final String LOG_FORMAT_SHORT = "HH:mm' - %s'";
+	
+	/**
+	 * Intent for the service.
+	 */
+	public static final String CLASSIFIER_SERVICE = "at.jku.pci.lazybird.CLASSIFIER_SERVICE";
+	// Extras
+	public static final String EXTRA_ACTIVITY_NAME = "at.jku.pci.lazybird.ACTIVITY_NAME";
+	public static final String EXTRA_LOG_ENTRY = "at.jku.pci.lazybird.LOG_ENTRY";
+	
+	/**
+	 * UID for the ongoing notification.
+	 */
+	private static final int NOTIFICATION_REPORTING = 64;
+	private static final int NOTIFICATION_WRITE_FAIL = 65;
+	private static final int NOTIFICATION_CONNECTION_FAIL = 66;
+	
+	/**
+	 * To determine from the main activity whether the service is running or was shut down.
+	 */
+	private static boolean sRunning = false;
+	
+	private final IBinder mBinder = new LocalBinder();
+	private NotificationManager mNotificationManager;
+	private SensorManager mSensorManager;
+	private LocalBroadcastManager mBrodcastManager;
+	private PendingIntent mNotificationIntent;
+	
+	private int mLastActivity;
+	private boolean mTextToSpeech;
+	private String mLanguage;
+	private boolean mWriteToFile;
+	private String mFilename;
+	private String mDirname;
+	private BufferedWriter mOutfile = null;
+	private SimpleDateFormat mDateFormat = null;
+	private boolean mReport;
+	private String mServer;
+	private String mUsername;
+	private CoordinatorClient mClient = null;
+	private Classifier mClassifier;
+	private Instances mHeader;
+	private int mFeatures;
+	private SlidingWindow mSlidingWindow;
+	private Date mStartTime;
+	
+	@Override
+	public void onCreate()
+	{
+		// Only one instance is created by the system
+		// (hopefully, the docs don't explicitly state this)
+		sInstance = this;
+		if(LOCAL_LOGV) Log.v(LOGTAG, "Service created: " + this);
+		
+		mNotificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		mBrodcastManager = LocalBroadcastManager.getInstance(this);
+		sRunning = false;
+		
+		// Make the pending intent bring the app to the front rather than starting a new activity
+		Intent i = new Intent(this, MainActivity.class);
+		i.setAction("android.intent.action.MAIN");
+		i.addCategory("android.intent.category.LAUNCHER");
+		i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+		mNotificationIntent = PendingIntent.getActivity(this, 0, i, 0);
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId)
+	{
+		if(LOCAL_LOGV) Log.v(LOGTAG, "Received start id " + startId + ": " + intent);
+		
+		// Service is restarted by the system, we don't want that
+		if(intent == null)
+		{
+			stopSelf();
+			return START_STICKY;
+		}
+		
+		if(!sRunning)
+		{
+			sRunning = true;
+			mStartTime = new Date();
+			
+			// get information from the intent
+			mClassifier =
+				(Classifier)intent.getSerializableExtra(ReportFragment.EXTRA_CLASSIFIER);
+			mFeatures = intent.getIntExtra(ReportFragment.EXTRA_FEATURES, 0x21);
+			mHeader = buildHeader(mFeatures);
+			int windowSize = intent.getIntExtra(ReportFragment.EXTRA_WINDOW, 1000);
+			int jumpSize = intent.getIntExtra(ReportFragment.EXTRA_JUMP, 100);
+			mSlidingWindow = new SlidingWindow(windowSize, jumpSize, this);
+			
+			if(intent.getBooleanExtra(ReportFragment.EXTRA_TTS, false))
+			{
+				mLanguage = intent.getStringExtra(ReportFragment.EXTRA_LANGUAGE);
+				setTextToSpeech(true);
+			}
+			
+			if(intent.getBooleanExtra(ReportFragment.EXTRA_WRITE_LOG, false))
+			{
+				mFilename = intent.getStringExtra(ReportFragment.EXTRA_FILENAME);
+				mDirname = intent.getStringExtra(ReportFragment.EXTRA_DIRNAME);
+				mDateFormat = new SimpleDateFormat(LOG_FORMAT, Locale.US);
+				setWriteToFile(true);
+				log(getString(R.string.rservice_started));
+			}
+			
+			if(intent.getBooleanExtra(ReportFragment.EXTRA_REPORT, false))
+			{
+				mServer = intent.getStringExtra(ReportFragment.EXTRA_REPORT_SERVER);
+				mUsername = intent.getStringExtra(ReportFragment.EXTRA_REPORT_USER);
+				setReportToServer(true);
+			}
+			
+			startReporting();
+		}
+		
+		return START_STICKY;
+	}
+
+	@Override
+	public void onDestroy()
+	{
+		if(LOCAL_LOGV) Log.v(LOGTAG, "Service destroyed: " + this);
+		
+		stopForeground(true);
+		if(mSensorManager != null)
+			mSensorManager.unregisterListener(this);
+		
+		try
+		{
+			if(mOutfile != null)
+			{
+				log(getString(R.string.rservice_stopped));
+				mOutfile.flush();
+				mOutfile.close();
+			}
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+		}
+		finally
+		{
+			mOutfile = null;
+		}
+		
+		if(mClient != null)
+		{
+			mClient.interrupt();
+		}
+		
+		// TODO stop text-to-speech
+		
+		Toast.makeText(this, R.string.rservice_stopped, Toast.LENGTH_SHORT).show();
+		sRunning = false;
+		sInstance = null;
+		mBrodcastManager.sendBroadcast(new Intent(ReportFragment.BCAST_SERVICE_STOPPED));
+	}
+	
+	@Override
+	public IBinder onBind(Intent intent)
+	{
+		return mBinder;
+	}
+	
+	public class LocalBinder extends Binder
+	{
+		ClassifierService getService()
+		{
+			return ClassifierService.this;
+		}
+	}
+	
+	private Instances buildHeader(int flags)
+	{
+		final Feature[] features = Feature.getFeatures(flags);
+		final String[] classes = getResources().getStringArray(R.array.classes);
+		final FastVector attributes = new FastVector(features.length + 1);
+		final FastVector values = new FastVector(classes.length);
+		
+		for(Feature f : features)
+			attributes.addElement(new Attribute(f.getName()));
+		for(int j = 1; j < classes.length; j++)
+			values.addElement(classes[j]);
+		attributes.addElement(new Attribute("class", values));
+		
+		final Instances out = new Instances("header", attributes, 2);
+		out.setClassIndex(out.numAttributes() - 1);
+		
+		return out;
+	}
+	
+	/**
+	 * Gets the last instance of the service created by the system.
+	 * 
+	 * @return the last instance of the service created, or {@code null} if none was created.
+	 */
+	public static ClassifierService getInstance()
+	{
+		return sInstance;
+	}
+	
+	/**
+	 * Gets a value indicating whether the service has received a start command and is running.
+	 * 
+	 * @return {@code true} if the service is running or was killed unexpectedly and
+	 *         {@link #onDestroy()} has not been called, {@code false} otherwise.
+	 */
+	public static boolean isRunning()
+	{
+		return sRunning;
+	}
+	
+	/**
+	 * Gets the last activity as classified by the classifier.
+	 * 
+	 * @return the name of the last activity.
+	 * @see #getClassifier()
+	 */
+	public String getLastActivity()
+	{
+		return mHeader.classAttribute().value(mLastActivity);
+	}
+	
+	/**
+	 * Gets a value indicating whether Text-to-speech output is enabled.
+	 * 
+	 * @return {@code true} if Text-to-speech output is enabled, {@code flase} otherwise.
+	 */
+	public boolean getTextToSpeech()
+	{
+		return mTextToSpeech;
+	}
+	
+	/**
+	 * Sets whether Text-to-speech output is enabled.
+	 * 
+	 * @param enabled {@code true} if Text-to-speech output should be enabled.
+	 */
+	public void setTextToSpeech(boolean enabled)
+	{
+		// TODO set text to speech
+	}
+	
+	/**
+	 * Gets a value indicating whether a log is written to the output file.
+	 * 
+	 * @return {@code true} if the log is written to the output file, {@code false} otherwise.
+	 * @see #setWriteToFile(boolean)
+	 */
+	public boolean getWriteToFile()
+	{
+		return mOutfile != null;
+	}
+	
+	/**
+	 * Sets whether the log should be written to a file. This has no effect if no filename was
+	 * specified when starting the service.
+	 * <p>
+	 * Note that every time this is set from {@code false} to {@code true}, the output file is
+	 * overwritten.
+	 * 
+	 * @param write {@code true} to write a log file.
+	 */
+	public void setWriteToFile(boolean write)
+	{
+		// No filename was specified with the intent, so nothing to do
+		if(!mWriteToFile)
+			return;
+		
+		if(mOutfile == null && write)
+		{
+			final File dir = new File(Environment.getExternalStorageDirectory(), mDirname);
+			if(!dir.exists())
+				dir.mkdir();
+			final File file = new File(dir, mFilename);
+			
+			try
+			{
+				mOutfile = new BufferedWriter(new FileWriter(file));
+				mOutfile.write(getString(R.string.app_name));
+				mOutfile.write(' ');
+				mOutfile.write(getString(R.string.rservice_label));
+				mOutfile.write(" Log\n\n");
+			}
+			catch(IOException ex)
+			{
+				notifyWriteFail();
+				mOutfile = null;
+			}
+		}
+		else if(mOutfile != null && !write)
+		{
+			try
+			{
+				mOutfile.flush();
+				mOutfile.close();
+			}
+			catch(IOException ex)
+			{
+				notifyWriteFail();
+			}
+			finally
+			{
+				mOutfile = null;
+			}
+		}
+	}
+	
+	/**
+	 * Gets the filename the log is written to.
+	 * 
+	 * @return the filename the log is written to.
+	 */
+	public String getFilename()
+	{
+		return mFilename;
+	}
+	
+	/**
+	 * Gets the directory the output file is written to. The base directory is always
+	 * {@link Environment#getExternalStorageDirectory()}.
+	 * 
+	 * @return the directory of the output file.
+	 * @see #getFilename()
+	 */
+	public String getDirname()
+	{
+		return mDirname;
+	}
+	
+	/**
+	 * Gets a value indicating whether the current activity is being reported to the server.
+	 * 
+	 * @return {@code true} if the activity is reported, {@code false} otherwise.
+	 * @see #setReportToServer(boolean)
+	 * @see #getReportServer()
+	 * @see #getReportUsername()
+	 */
+	public boolean getReportToServer()
+	{
+		return mClient != null;
+	}
+	
+	/**
+	 * Sets whether the current activity should be reported to the sever. This has no effect if
+	 * no server was specified when starting the service.
+	 * 
+	 * @param report {@code true} to report the current activity.
+	 * @see #getReportToServer()
+	 * @see #getReportServer()
+	 * @see #getReportUsername()
+	 */
+	public void setReportToServer(boolean report)
+	{
+		// No server and user specified with the intent, so nothing to do
+		if(!mReport)
+			return;
+		
+		// If client died, remove it
+		if(mClient != null && !mClient.isAlive())
+			mClient = null;
+		
+		if(mClient == null && report)
+		{
+			String host;
+			int port;
+			
+			if(mServer == null || mServer.isEmpty())
+			{
+				host = CoordinatorClient.DEFAULT_SERVER_HOST;
+				port = CoordinatorClient.DEFAULT_SERVER_PORT;
+			}
+			else if(mServer.contains(":"))
+			{
+				final int idx = mServer.indexOf(":");
+				host = mServer.substring(0, idx);
+				try
+				{
+					port = Integer.parseInt(mServer.substring(idx + 1, mServer.length()));
+				}
+				catch(NumberFormatException ex)
+				{
+					port = CoordinatorClient.DEFAULT_SERVER_PORT;
+				}
+			}
+			else
+			{
+				host = mServer;
+				port = CoordinatorClient.DEFAULT_SERVER_PORT;
+			}
+			
+			mClient = new CoordinatorClient(host, port, mUsername);
+			
+			try
+			{
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException ex)
+			{
+				// Why would I be interrupted?
+			}
+			
+			if(!mClient.isAlive())
+			{
+				notifyConnectionFail();
+				mClient = null;
+			}
+		}
+		else if(mClient != null && !report)
+		{
+			mClient.interrupt();
+			mClient = null;
+		}
+		
+	}
+	
+	/**
+	 * Gets the server the current activity is reported to, if enabled.
+	 * 
+	 * @return the server to report to, or {@code null} if not set.
+	 * @see #getReportToServer()
+	 * @see #getReportUsername()
+	 */
+	public String getReportServer()
+	{
+		return mServer;
+	}
+	
+	/**
+	 * Gets the username used to report the current activity, if enabled.
+	 * 
+	 * @return the username, or {@code null} if not set.
+	 * @see #getReportToServer()
+	 * @see #getReportServer()
+	 */
+	public String getReportUsername()
+	{
+		return mUsername;
+	}
+	
+	/**
+	 * Gets the classifier used to classify the current activity.
+	 */
+	public Classifier getClassifier()
+	{
+		return mClassifier;
+	}
+	
+	/**
+	 * Gets the features used for classifying the current activity.
+	 * 
+	 * @return
+	 */
+	public int getFeatures()
+	{
+		return mFeatures;
+	}
+	
+	/**
+	 * Gets the start time of the service.
+	 * 
+	 * @return the start time of the service, that is the time at which a start command was
+	 *         received.
+	 */
+	public Date getStartTime()
+	{
+		return mStartTime;
+	}
+	
+	/**
+	 * Builds the ongoing notification for this service.
+	 * 
+	 * @return the ongoing notification for this service.
+	 */
+	private Notification makeOngoingNotification()
+	{
+		CharSequence text = getText(R.string.rservice_started);
+		CharSequence title = getText(R.string.app_name);
+		
+		Notification n = new Notification.Builder(this)
+			.setTicker(text)
+			.setContentTitle(title)
+			.setContentText(text)
+			.setContentIntent(mNotificationIntent)
+			.setSmallIcon(R.drawable.ic_stat_rservice)
+			.setOngoing(true)
+			.setLights(0xFFFFFF00, 500, 500)
+			.setAutoCancel(false).getNotification();
+		
+		n.flags |= Notification.FLAG_SHOW_LIGHTS;
+		return n;
+	}
+	
+	/**
+	 * Builds and shows a notification, notifying the user of an error when writing the output
+	 * log file.
+	 */
+	private void notifyWriteFail()
+	{
+		Resources res = getResources();
+		CharSequence ticker = res.getText(R.string.write_fail);
+		CharSequence text = res.getText(R.string.write_fail_long);
+		CharSequence title = res.getText(R.string.app_name);
+		
+		Notification n = new Notification.Builder(this)
+			.setTicker(ticker)
+			.setContentTitle(title)
+			.setContentText(text)
+			.setSmallIcon(R.drawable.ic_stat_rservice)
+			.setAutoCancel(true)
+			.setContentIntent(mNotificationIntent).getNotification();
+		
+		mNotificationManager.notify(NOTIFICATION_WRITE_FAIL, n);
+	}
+	
+	/**
+	 * Builds and shows a notification, notifying the user of a failed connection to the
+	 * reporting server.
+	 */
+	private void notifyConnectionFail()
+	{
+		Resources res = getResources();
+		CharSequence ticker = res.getText(R.string.conn_fail);
+		CharSequence text = res.getText(R.string.conn_fail_long);
+		CharSequence title = res.getText(R.string.app_name);
+		
+		Notification n = new Notification.Builder(this)
+			.setTicker(ticker)
+			.setContentTitle(title)
+			.setContentText(text)
+			.setSmallIcon(R.drawable.ic_stat_rservice)
+			.setAutoCancel(true)
+			.setContentIntent(mNotificationIntent).getNotification();
+		
+		mNotificationManager.notify(NOTIFICATION_CONNECTION_FAIL, n);
+	}
+	
+	private void startReporting()
+	{
+		Sensor s = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		mSensorManager.registerListener(this, s, SensorManager.SENSOR_DELAY_FASTEST);
+		
+		startForeground(NOTIFICATION_REPORTING, makeOngoingNotification());
+		mBrodcastManager.sendBroadcast(new Intent(RecorderFragment.BCAST_SERVICE_STARTED));
+	}
+	
+	/**
+	 * Writes the specified message to the log file, if enabled.
+	 * 
+	 * @param msg the message to write
+	 */
+	private void log(String msg)
+	{
+		if(mOutfile == null)
+			return;
+		
+		try
+		{
+			mOutfile.write(String.format(mDateFormat.format(new Date()), msg));
+			mOutfile.write("\n");
+		}
+		catch(IOException ex)
+		{
+			notifyWriteFail();
+			mOutfile = null;
+		}
+	}
+	
+	private void onActivityChanged(int newActivity)
+	{
+		mLastActivity = newActivity;
+		
+		final String activity = getLastActivity();
+		mBrodcastManager.sendBroadcast(new Intent(ReportFragment.BCAST_NEW_ACTIVITY)
+			.putExtra(EXTRA_ACTIVITY_NAME, activity));
+		
+		log(getString(R.string.log_new_activity, activity));
+		
+		if(mTextToSpeech)
+		{
+			// TODO output with TTS
+		}
+		
+		if(mClient != null)
+		{
+			if(mClient.isAlive())
+			{
+				mClient.setCurrentActivity(ClassLabel.parse(activity));
+			}
+			else
+			{
+				notifyConnectionFail();
+				mClient = null;
+			}
+		}
+	}
+	
+	@Override
+	public void onSensorChanged(SensorEvent event)
+	{
+		Instance i = new Instance(4);
+		i.setValue(0, System.currentTimeMillis());
+		for(int j = 0; j < 3; j++)
+			i.setValue(j + 1, event.values[j]);
+		mSlidingWindow.add(i);
+	}
+	
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy)
+	{
+		
+	}
+	
+	@Override
+	public void onWindowChanged(Iterable<Instance> window)
+	{
+		Instance i = FeatureExtractor.extractFeatures(window, mFeatures);
+		mHeader.delete();
+		mHeader.add(i);
+		
+		try
+		{
+			// TODO maybe apply some averaging
+			double tmp = mClassifier.classifyInstance(i);
+			if(tmp == Instance.missingValue())
+				return;
+			if(mLastActivity != (int)tmp)
+				onActivityChanged((int)tmp);
+		}
+		catch(Exception ex)
+		{
+			mLastActivity = -1;
+		}
+	}
+	
+}
--- LazyBird/src/at/jku/pci/lazybird/ReportFragment.java
+++ LazyBird/src/at/jku/pci/lazybird/ReportFragment.java
@@ -1,11 +1,17 @@
 package at.jku.pci.lazybird;
 
 import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Environment;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,13 +20,34 @@ import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 import weka.classifiers.Classifier;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.StreamCorruptedException;
 
 public class ReportFragment extends Fragment
 {
+	// Extras
+	public static final String EXTRA_CLASSIFIER = "at.jku.pci.lazybird.CLASSIFIER";
+	public static final String EXTRA_WINDOW = "at.jku.pci.lazybird.WINDOW";
+	public static final String EXTRA_JUMP = "at.jku.pci.lazybird.JUMP";
+	public static final String EXTRA_FEATURES = "at.jku.pci.lazybird.FEATURES";
+	public static final String EXTRA_TTS = "at.jku.pci.lazybird.TTS";
+	public static final String EXTRA_LANGUAGE = "at.jku.pci.lazybird.LANGUAGE";
+	public static final String EXTRA_WRITE_LOG = "at.jku.pci.lazybird.WRITE_LOG";
+	public static final String EXTRA_FILENAME = "at.jku.pci.lazybird.LOG_FILENAME";
+	public static final String EXTRA_DIRNAME = "at.jku.pci.lazybird.LOG_DIRNAME";
+	public static final String EXTRA_REPORT = "at.jku.pci.lazybird.REPORT";
+	public static final String EXTRA_REPORT_SERVER = "at.jku.pci.lazybird.REPORT_SERVER";
+	public static final String EXTRA_REPORT_USER = "at.jku.pci.lazybird.REPORT_USER";
 	// Intents
 	public static final String BCAST_SERVICE_STOPPED = "at.jku.pci.lazybird.REP_SERVICE_STOPPED";
 	public static final String BCAST_SERVICE_STARTED = "at.jku.pci.lazybird.REP_SERVICE_STARTED";
+	public static final String BCAST_NEW_ACTIVITY = "at.jku.pci.lazybird.REP_NEW_ACTIVITY";
 	// Constants
 	public static final String LOGTAG = "ReportFragment";
 	public static final boolean LOCAL_LOGV = true;
@@ -56,12 +83,30 @@ public class ReportFragment extends Fragment
 	private SharedPreferences mPrefsClassifier;
 	
 	// Fields
+	TextView mLblNoClassifier;
 	Switch mSwClassifiy;
 	CheckBox mChkTts;
 	CheckBox mChkReport;
 	ListView mListLog;
 	
 	private Classifier mClassifier = null;
+	// Handlers
+	// private ClassifierService mService = null;
+	private LocalBroadcastManager mBroadcastManager;
+	private IntentFilter mServiceIntentFilter;
+	private BroadcastReceiver mServiceReceiver = new BroadcastReceiver() {
+		public void onReceive(Context context, Intent intent)
+		{
+			if(LOCAL_LOGV) Log.v(LOGTAG, "Received broadcast: " + intent);
+			
+			if(intent.getAction().equals(BCAST_SERVICE_STOPPED))
+				onServiceStopped();
+			else if(intent.getAction().equals(BCAST_SERVICE_STARTED))
+				onServiceStarted();
+			else if(intent.getAction().equals(BCAST_NEW_ACTIVITY))
+				onNewActivity(intent);
+		}
+	};
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -73,15 +118,25 @@ public class ReportFragment extends Fragment
 	}
 	
 	@Override
-	public void onViewCreated(View view, Bundle savedInstanceState)
+	public void onActivityCreated(Bundle savedInstanceState)
 	{
-		super.onViewCreated(view, savedInstanceState);
+		super.onActivityCreated(savedInstanceState);
+		
+		mBroadcastManager = LocalBroadcastManager.getInstance(getActivity());
+		mServiceIntentFilter = new IntentFilter();
+		mServiceIntentFilter.addAction(BCAST_SERVICE_STARTED);
+		mServiceIntentFilter.addAction(BCAST_SERVICE_STOPPED);
 		
 		mPrefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
 		mPrefsClassifier = Storage.getClassifierPreferences(getActivity());
 		readSettings();
 		
 		getWidgets(getView());
+		checkForClassifier();
+		
+		// if the service is running and we just got created, fill inputs with running data.
+		// setting of input enabled and such things are done in onResume.
+		// TODO get classifier from service
 	}
 	
 	/**
@@ -91,7 +146,8 @@ public class ReportFragment extends Fragment
 	 */
 	private void getWidgets(View v)
 	{
-		// TODO disable everything and enable in code
+		mLblNoClassifier = (TextView)v.findViewById(R.id.lblNoClassifier);
+		
 		mSwClassifiy = (Switch)v.findViewById(R.id.swClassify);
 		mSwClassifiy.setOnCheckedChangeListener(onSwClassifyCheckedChange);
 		
@@ -101,6 +157,24 @@ public class ReportFragment extends Fragment
 		mChkReport.setOnCheckedChangeListener(onChkReportCheckedChange);
 		
 		mListLog = (ListView)v.findViewById(R.id.listLog);
+	}
+	
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+		mBroadcastManager.unregisterReceiver(mServiceReceiver);
+	}
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+		
+		// check for running service every time the fragment is resumed, since broadcast can't
+		// be received while paused or stopped
+		//mSwClassifiy.setChecked(ClassifierService.isRunning());
+		mBroadcastManager.registerReceiver(mServiceReceiver, mServiceIntentFilter);
 	}
 	
 	/**
@@ -126,33 +200,103 @@ public class ReportFragment extends Fragment
 		sTrainedFeatures = mPrefsClassifier.getInt(Storage.KEY_FEATURES, 0);
 	}
 	
-	private OnCheckedChangeListener onSwClassifyCheckedChange =
-		new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-			{
-				// TODO Auto-generated method stub
-				
-			}
-		};
+	private void setClassifierPresent(boolean present)
+	{
+		mLblNoClassifier.setVisibility(present ? View.GONE : View.VISIBLE);
+		mSwClassifiy.setEnabled(present);
+		if(!present)
+			mClassifier = null;
+	}
 	
-	private OnCheckedChangeListener onChkTtsCheckedChange =
-		new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+	private void checkForClassifier()
+	{
+		readSettings();
+		boolean fileSet = (sClassifierFile != null && !sClassifierFile.isEmpty());
+		mSwClassifiy.setEnabled(fileSet);
+		
+		if(fileSet)
+		{
+			try
 			{
-				// TODO Auto-generated method stub
-				
+				InputStream is = getActivity().openFileInput(sClassifierFile);
+				ObjectInputStream ois = new ObjectInputStream(is);
+				mClassifier = (Classifier)ois.readObject();
+				ois.close();
+				setClassifierPresent(true);
 			}
-		};
+			catch(FileNotFoundException ex)
+			{
+				Log.w(LOGTAG, "Classifier file from preference not found.", ex);
+				sClassifierFile = "";
+				mPrefsClassifier.edit().putString(Storage.KEY_CLASSIFIER_FILE, "").apply();
+				setClassifierPresent(false);
+			}
+			catch(StreamCorruptedException ex)
+			{
+				Log.e(LOGTAG, "Serialized classifier is corrupted.", ex);
+				sClassifierFile = "";
+				mPrefsClassifier.edit().putString(Storage.KEY_CLASSIFIER_FILE, "").apply();
+				setClassifierPresent(false);
+			}
+			catch(IOException ex)
+			{
+				Toast.makeText(getActivity(), R.string.error_io, Toast.LENGTH_LONG).show();
+				Log.e(LOGTAG, "IOException while reading serialized classifier.", ex);
+				setClassifierPresent(false);
+			}
+			catch(ClassNotFoundException ex)
+			{
+				Log.wtf(LOGTAG, "Class of the serialized classifier not found??", ex);
+				throw new InternalError();
+			}
+		}
+		else
+		{
+			setClassifierPresent(false);
+		}
+	}
 	
-	private OnCheckedChangeListener onChkReportCheckedChange =
-		new CompoundButton.OnCheckedChangeListener() {
-			@Override
-			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
-			{
-				// TODO Auto-generated method stub
-				
-			}
-		};
+	private OnCheckedChangeListener onSwClassifyCheckedChange = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+		{
+			// TODO Auto-generated method stub
+		}
+	};
+	
+	private OnCheckedChangeListener onChkTtsCheckedChange = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	private OnCheckedChangeListener onChkReportCheckedChange = new OnCheckedChangeListener() {
+		@Override
+		public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+		{
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	private void onNewActivity(Intent intent)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void onServiceStarted()
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void onServiceStopped()
+	{
+		// TODO Auto-generated method stub
+		
+	}
 }
