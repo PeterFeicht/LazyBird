4c94f3db48e0507783ca657da4bcc2c0ecdab0a6
Peter <Peter@8ce2c88a-9d02-4b18-af08-0d329a8e4d64>
2012-12-09 21:04:33 +0000

    LazyBird: -> Implemented and enabled Text-to-speech output.

--- trunk/LazyBird/src/at/jku/pci/lazybird/ReportFragment.java
+++ trunk/LazyBird/src/at/jku/pci/lazybird/ReportFragment.java
@@ -212,7 +212,6 @@ public class ReportFragment extends Fragment
 		
 		mChkTts = (CheckBox)v.findViewById(R.id.chkTts);
 		mChkTts.setOnCheckedChangeListener(onChkTtsCheckedChange);
-		mChkTts.setEnabled(false);
 		mChkReport = (CheckBox)v.findViewById(R.id.chkReport);
 		mChkReport.setOnCheckedChangeListener(onChkReportCheckedChange);
 		
