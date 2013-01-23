059824fa9ba662d90844f21b70f3f49f6e1c0729
pezo <pezo@8ce2c88a-9d02-4b18-af08-0d329a8e4d64>
2013-01-23 15:11:00 +0000

    -> Added message when connection to the server fails. -> Added progress bar while waiting for the first server update. -> Changed show offline to be checked by default. -> Enabled animation for user container layout changes. -> Implemented live view.

--- trunk/LazyBird/src/at/jku/pci/lazybird/LiveViewActivity.java
+++ trunk/LazyBird/src/at/jku/pci/lazybird/LiveViewActivity.java
@@ -1,25 +1,37 @@
 package at.jku.pci.lazybird;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ProgressBar;
 import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.TextView;
+import at.jku.pci.lazybird.util.UserActivityView;
+import at.jku.pervasive.sd12.actclient.CoordinatorClient;
+import at.jku.pervasive.sd12.actclient.CoordinatorClient.UserState;
+import at.jku.pervasive.sd12.actclient.GroupStateListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
-public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNavigationListener
+public class LiveViewActivity extends Activity implements ActionBar.OnNavigationListener,
+		GroupStateListener
 {
 	// Constants
 	public static final String LOGTAG = "LiveViewActivity";
 	public static final boolean LOCAL_LOGV = true;
-	public static final int AUTO_UPDATE_DELAY = 250;
+	public static final int AUTO_UPDATE_DELAY = 500;
+	public static final String STATE_VIEW_USERS = "at.jku.pci.lazybird.STATE_VIEW_USERS";
+	public static final String STATE_USER_VIEWS = "at.jku.pci.lazybird.STATE_USER_VIEWS";
 	// Settings
 	/**
 	 * Setting: {@link SettingsActivity#KEY_REPORT_SERVER}
@@ -35,15 +47,24 @@ public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNa
 	// Views
 	private FlowLayout mUserContainer;
 	private CheckBox mChkShowOffline;
+	private TextView mLblNoUsername;
+	private TextView mLblCannotConnect;
+	private ProgressBar mProgressServerUpdate;
 	
 	// Fields
 	private ArrayAdapter<String> mViewUsers;
+	private HashMap<String, UserActivityView> mUserViews;
+	private CoordinatorClient mClient = null;
 	// Handlers
 	private Handler mHandler = new Handler();
-	private Runnable mRunUpdateValues = new Runnable() {
+	private Runnable mRunUpdateAges = new Runnable() {
 		public void run()
 		{
-			// TODO Update ages
+			// When there are no updates from the server in a long time, the displayed ages are
+			// incremented until the server sends an update
+			for(UserActivityView v : mUserViews.values())
+				v.setAge(v.getAge() + AUTO_UPDATE_DELAY);
+			mHandler.postDelayed(mRunUpdateAges, AUTO_UPDATE_DELAY);
 		}
 	};
 	
@@ -52,6 +73,7 @@ public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNa
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_live_view);
+		mUserViews = new HashMap<String, UserActivityView>(20);
 		
 		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 		readSettings();
@@ -79,18 +101,46 @@ public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNa
 		
 		mChkShowOffline = (CheckBox)findViewById(R.id.chkShowOffline);
 		mChkShowOffline.setOnCheckedChangeListener(onChkShowOfflineCheckedChange);
+		
+		mLblNoUsername = (TextView)findViewById(R.id.lblNoUsername);
+		mLblNoUsername.setVisibility(sReportUser.isEmpty() ? View.VISIBLE : View.GONE);
+		
+		mLblCannotConnect = (TextView)findViewById(R.id.lblCannotConnect);
+		
+		mProgressServerUpdate = (ProgressBar)findViewById(R.id.progressServerUpdate);
 	}
 	
 	@Override
 	protected void onResume()
 	{
 		super.onResume();
+		
+		mLblCannotConnect.setVisibility(View.GONE);
+		
+		if(sReportUser.isEmpty())
+		{
+			mLblNoUsername.setVisibility(View.VISIBLE);
+			mChkShowOffline.setEnabled(false);
+			mUserContainer.removeAllViews();
+		}
+		else
+		{
+			mLblNoUsername.setVisibility(View.GONE);
+			mChkShowOffline.setEnabled(true);
+			
+			connect();
+		}
 	}
 	
 	@Override
 	protected void onPause()
 	{
 		super.onPause();
+		
+		if(mClient != null)
+			mClient.interrupt();
+		mClient = null;
+		mHandler.removeCallbacks(mRunUpdateAges);
 	}
 	
 	/**
@@ -102,11 +152,65 @@ public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNa
 		sReportUser = mPrefs.getString(SettingsActivity.KEY_REPORT_USER, "");
 	}
 	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
+	/**
+	 * Tries to connect to the server, setting lblCannotConnect to visible if that fails.
+	 */
+	private void connect()
 	{
-		//getMenuInflater().inflate(R.menu.activity_live_view, menu);
-		return true;
+		String host;
+		int port;
+		
+		if(sReportServer.isEmpty())
+		{
+			// Use default server and port in case something is missing
+			host = CoordinatorClient.DEFAULT_SERVER_HOST;
+			port = CoordinatorClient.DEFAULT_SERVER_PORT;
+		}
+		else if(sReportServer.contains(":"))
+		{
+			// Use specified server and port
+			final int idx = sReportServer.indexOf(":");
+			host = sReportServer.substring(0, idx);
+			try
+			{
+				port = Integer.parseInt(
+					sReportServer.substring(idx + 1, sReportServer.length()));
+			}
+			catch(NumberFormatException ex)
+			{
+				port = CoordinatorClient.DEFAULT_SERVER_PORT;
+			}
+		}
+		else
+		{
+			// Port is missing, use default
+			host = sReportServer;
+			port = CoordinatorClient.DEFAULT_SERVER_PORT;
+		}
+		
+		// Start reporting
+		if(LOCAL_LOGV) Log.v(LOGTAG, "Connecting to server...");
+		mClient = new CoordinatorClient(host, port, sReportUser);
+		try
+		{
+			Thread.sleep(1000);
+		}
+		catch(InterruptedException ex)
+		{
+		}
+		
+		// In case the connection fails, the thread of the client stops; check
+		if(mClient.isAlive())
+		{
+			mClient.addGroupStateListener(this);
+			if(LOCAL_LOGV) Log.v(LOGTAG, "Connected.");
+		}
+		else
+		{
+			if(LOCAL_LOGV) Log.v(LOGTAG, "Connection failed.");
+			mLblCannotConnect.setVisibility(View.VISIBLE);
+			mClient = null;
+		}
 	}
 	
 	@Override
@@ -134,7 +238,60 @@ public class LiveViewActivity extends FragmentActivity implements ActionBar.OnNa
 			@Override
 			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
 			{
-				// TODO Show/hide offline users
+				for(UserActivityView v : mUserViews.values())
+					v.setShowOffline(mChkShowOffline.isChecked());
 			}
 		};
+	
+	@Override
+	public void groupStateChanged(UserState[] groupState)
+	{
+		if(groupState == null)
+			return;
+		
+		mHandler.removeCallbacks(mRunUpdateAges);
+		
+		// We don't need to remove users from our list, since the server doesn't drop users
+		final HashSet<UserState> newUsers = new HashSet<UserState>(groupState.length);
+		for(UserState u : groupState)
+		{
+			UserActivityView v = mUserViews.get(u.getUserId());
+			if(v != null)
+			{
+				v.setAge(u.getUpdateAge());
+				v.setActivity(u.getActivity());
+			}
+			else
+				newUsers.add(u);
+		}
+		
+		// Create views for the new users and add them to our list
+		final ArrayList<UserActivityView> newViews =
+			new ArrayList<UserActivityView>(newUsers.size());
+		for(UserState u : newUsers)
+		{
+			UserActivityView v = new UserActivityView(this);
+			v.setText(u.getUserId());
+			v.setAge(u.getUpdateAge());
+			v.setActivity(u.getActivity());
+			v.setShowOffline(mChkShowOffline.isChecked());
+			mUserViews.put(u.getUserId(), v);
+			newViews.add(v);
+			// TODO add user to dropdown
+		}
+		
+		runOnUiThread(new Runnable() {
+			@Override
+			public void run()
+			{
+				mProgressServerUpdate.setVisibility(View.GONE);
+				for(UserActivityView v : newViews)
+					mUserContainer.addView(v);
+			}
+		});
+		
+		// TODO sort users
+		// maybe remove offline users from view user list
+		mHandler.postDelayed(mRunUpdateAges, AUTO_UPDATE_DELAY);
+	}
 }
